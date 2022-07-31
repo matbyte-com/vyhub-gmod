465e886ab9c142cf5f144adcfaa95f9943ff1310
Jakob Mueller <jakob.mueller@ibot3.de>
2022-07-31 13:53:47 +0200

    Improve sh_config

--- lua/vyhub/config/sh_config.lua
+++ lua/vyhub/config/sh_config.lua
@@ -1,7 +1,12 @@
-VyHub.Config.debug = true
+-- Language used. Currently only languages in the "lang" directory are supported.
 VyHub.Config.lang = "en"
 
--- Enable chat tags
+-- Enable chat tags, not working together with DarkRP
 VyHub.Config.chat_tags = true
 
+-- Date format to use (for example in ban message)
+-- See https://tieske.github.io/date/#dateObject.fmt
 VyHub.Config.date_format = "%Y-%m-%d %H:%M:%S %z"
+
+-- Debug mode. Enable to see all API requests and responses
+VyHub.Config.debug = false
