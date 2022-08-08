2093b86f7c7857dff180e81a892c1a98ed31b356
Jakob Mueller <jakob.mueller@ibot3.de>
2022-08-08 16:15:09 +0200

    Prevent low refresh intervals

--- lua/vyhub/shared/sh_01_config_defaults.lua
+++ lua/vyhub/shared/sh_01_config_defaults.lua
@@ -1,3 +1,14 @@
 VyHub.Config.date_format = VyHub.Config.date_format or "%Y-%m-%d %H:%M:%S %z"
-VyHub.Config.advert_interval = VyHub.Config.advert_interval or 180 
-VyHub.Config.advert_prefix = "[★] " 
+
+if SERVER then
+    VyHub.Config.advert_interval = VyHub.Config.advert_interval or 180 
+    VyHub.Config.advert_prefix = "[★] " 
+
+    -- Do not allow too small refresh intervals
+    if VyHub.Config.player_refresh_time < 5 then
+        VyHub.Config.player_refresh_time = 5
+    end
+    if VyHub.Config.group_refresh_time < 5 then
+        VyHub.Config.group_refresh_time = 5
+    end
+end
